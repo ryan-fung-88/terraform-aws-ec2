5a12752eaeef362802e224e525fe48d272ac5893
Fung <ryan.fung@accenture.com>
2024-02-16 10:39:25 -0600

    var change

--- variables.tf
+++ variables.tf
@@ -6,37 +6,6 @@ variable "ami" {
 variable "instance_type" {
   description = "The type of instance to start"
   type        = string
-
-  validation {
-    condition = contains([
-      "t3.nano",
-      "t3.micro",
-      "t3.small",
-      "t3.medium",
-      "t3.large",
-      "t3.xlarge",
-      "t3.2xlarge",
-      "m5.large",
-      "m5.xlarge",
-      "m5.2xlarge",
-      "m5.4xlarge",
-      "m5.8xlarge",
-      "m5.12xlarge",
-      "m5.16xlarge",
-      "m5.24xlarge",
-      "m5.metal",
-      "r5.large",
-      "r5.xlarge",
-      "r5.2xlarge",
-      "r5.4xlarge",
-      "r5.8xlarge",
-      "r5.12xlarge",
-      "r5.16xlarge",
-      "r5.24xlarge",
-      "r5.metal",
-    ], var.instance_type)
-    error_message = "Please refernce to the Instance Type and Size document"
-  }
 }
 
 variable "ebs_device_name" {
